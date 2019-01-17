ca91425d2f73b1f0c0044cd0f04dc43f8ed36522
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-17 19:15:58 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,8 +4,7 @@ def turn_count(board)
   board.each do |index| 
     if index == "X" || index == "O"
       counter += 1
-      number_of_turns = counter
     end 
-    return number_of_turns
+    counter
     end
 end
